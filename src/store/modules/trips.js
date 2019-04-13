bd23a75d3cf1f027b037aea7ef291bee66d7bec3
Taylor Dennis <tstall17@gmail.com>
2019-04-12 19:37:30 -0500

    [trip.js] add initial state, mutations, and actions

--- src/store/modules/trips.js
+++ src/store/modules/trips.js
@@ -1,24 +1,36 @@
 import { firebase } from '@/main'
 
+// TODO: initial state, mutations, getters
+
+const initialState = {
+    trip: {
+        events: {},
+        members: {},
+        tripId: '',
+        eventsId: '',
+        membersId: '',
+    },
+}
+
 const actions = {
-    addEvents: async (__, { tripId, eventsId, events }) => {
+    addEvents: async ({ commit }, { tripId, eventsId, events }) => {
         const addEvents = firebase.functions().httpsCallable('tripAddEvents')
         await addEvents({
             tripId,
             eventsId,
             events,
         }).then(function (result) {
-            console.log(result)
+            commit('setEvents', result)
         })
     },
-    addMembers: async (__, { tripId, membersId, events }) => {
+    addMembers: async ({ commit }, { tripId, membersId, events }) => {
         const addMembers = firebase.functions().httpsCallable('tripAddMembers')
         await addMembers({
             tripId,
             membersId,
             events,
         }).then(function (result) {
-            console.log(result)
+            commit('setMembers',result)
         })
     },
     createTrip: async (__, { userId, name, description, start, end }) => {
@@ -33,9 +45,34 @@ const actions = {
             console.log(result)
         })
     },
+    getTrip: async (__, { userId, name, description, start, end }) => {
+        const create = firebase.functions().httpsCallable('tripCreate')
+        await create({
+            userId,
+            name,
+            description,
+            start,
+            end
+        }).then(function (result) {
+            console.log(result)
+        })
+    },
 };
 
+const mutations = {
+    setEvents(state, payload) {
+        return state.trip.events = {...payload};
+    },
+    setMembers(state, payload) {
+        return state.trip.members = {...payload};
+    },
+    setTrip(state, payload) {
+        return state.trip = {...payload};
+    }
+}
 
 export default {
     actions,
+    mutations,
+    initialState,
 };
